9a5ca0267a0b68f2411dc1cfdc3dc69f75d25331
Andrei Panov <panovdev@gmail.com>
2023-05-28 08:49:36 +0200

    Initial Commit

---
+++ UberSwiftUITutorial/UberSwiftUITutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUITutorialApp.swift
+//  UberSwiftUITutorial
+//
+//  Created by Andrey Panov on 28/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUITutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
